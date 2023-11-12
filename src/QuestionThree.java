4fca5dd14dab93d1cfec88634881e997752056fb
abressam <amandabressam@gmail.com>
2023-11-12 12:42:08 -0300

    feat: fooBaaPrintByRange added to question three

--- src/QuestionThree.java
+++ src/QuestionThree.java
@@ -1,4 +1,23 @@
 public class QuestionThree {
+    public void fooBaaPrintByRange() {
+        for (int i = 1; i <= 100; i++) {
+            String resp = "";
+
+            if (this.multipleValidator(i, 3)) {
+                resp += "Foo";
+            }
+
+            if (this.multipleValidator(i, 5)) {
+                resp += "Baa";
+            }
+
+            if (resp.isEmpty()) {
+                resp += i;
+            }
+
+            System.out.println(resp);
+        }
+    }
     private boolean multipleValidator(int number, int multiple) {
         return number % multiple == 0;
     }

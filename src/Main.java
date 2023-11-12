d845ff204c96d2175445c8943dbc8b55ea9242e0
abressam <amandabressam@gmail.com>
2023-11-12 14:28:43 -0300

    feat: QuestionFour added to Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import QuestionFour.QuestionFour;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        QuestionThree questionThree = new QuestionThree();
-        questionThree.fooBaaPrintByRange();
+//        QuestionThree questionThree = new QuestionThree();
+//        questionThree.fooBaaPrintByRange();
+
+        Scanner scanner = new Scanner(System.in);
+
+        String s = scanner.nextLine().trim();
+        String t = scanner.nextLine().trim();
+        int k = Integer.parseInt(scanner.nextLine().trim());
+
+        QuestionFour questionFour = new QuestionFour();
+        String result = questionFour.ConcatRemove(s, t, k);
+
+        System.out.println(result);
     }
 }

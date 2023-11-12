9d34837718a0def5488c9047e8bd05ddd395dfa2
abressam <amandabressam@gmail.com>
2023-11-12 12:44:42 -0300

    feat: QuestionThree added to Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        QuestionThree questionThree = new QuestionThree();
+        questionThree.fooBaaPrintByRange();
     }
 }

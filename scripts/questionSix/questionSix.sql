bb557dbdd526e80355c2e61cca42f036ea994eae
abressam <amandabressam@gmail.com>
2023-11-12 16:30:58 -0300

    feat: create table and insert added to questionSix

---
+++ scripts/questionSix/questionSix.sql
@@ -0,0 +1,34 @@
+CREATE TABLE Students (
+    ID INTEGER,
+    Name VARCHAR(255),
+    Value INTEGER,
+    PRIMARY KEY (ID)
+);
+
+INSERT INTO Students (ID, Name, Value) VALUES
+    (1, 'Julia', 81),
+    (2, 'Carol', 68),
+    (3, 'Maria', 99),
+    (4, 'Andreia', 78),
+    (5, 'Jaqueline', 63),
+    (6, 'Marcela', 88);
+
+CREATE TABLE Notes (
+    Grade INTEGER,
+    Min_Value INTEGER,
+    Max_Value INTEGER,
+    PRIMARY KEY (Grade)
+);
+
+INSERT INTO Notes (Grade, Min_Value, Max_Value) VALUES
+    (1, 0, 9),
+    (2, 10, 19),
+    (3, 20, 29),
+    (4, 30, 39),
+    (5, 40, 49),
+    (6, 50, 59),
+    (7, 60, 69),
+    (8, 70, 79),
+    (9, 80, 89),
+    (10, 90, 100);
+
